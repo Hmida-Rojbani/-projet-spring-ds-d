35fa214863a3e9a72d0b361b3a11fe9164ef628b
sawssenOues <119492807+sawssenOues@users.noreply.github.com>
2022-12-26 15:42:54 +0100

    Update Student.java

--- src/main/java/de/tekup/studentsabsence/entities/Student.java
+++ src/main/java/de/tekup/studentsabsence/entities/Student.java
@@ -5,11 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 @Entity
 @Data
 @AllArgsConstructor
@@ -20,16 +20,30 @@ public class Student implements Serializable {
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long sid;
+    @Column(length = 50,nullable = false)
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String firstName;
+    @Size(max = 50)
     private String lastName;
+    @Size(max = 50)
     private String email;
+    @Column(unique = true,length = 8)
+    @Pattern(regexp = "^[0-9]{8}$",message = "phone number must contains only digits")
+    @Size(min = 8,max = 8, message = "phone number must be exactly 8 digits")
     private String phone;
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate dob;
 
     //TODO Complete Relations with other entities
-
-
+    @OneToMany(mappedBy = "student")
+    List<Absence> absences;
+    @ManyToOne
+    private Group group;
+    @OneToOne(mappedBy = "student")
+    private Image image;
+    
 
 }
